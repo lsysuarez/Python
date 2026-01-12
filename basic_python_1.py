391a6481bcba48aadc7d76df558d53fccb1fba02
Leiziana <leizsteffanysuarez@gmail.com>
2026-01-12 20:54:13 +0800

    Create basic_python_1.py
    First beginner Python project with:
    - interactive inputs
    - prints

---
+++ basic_python_1.py
@@ -0,0 +1,17 @@
+print("Hello World!")
+
+name = input("What is your name? ")
+age = input("How old are you? ")
+why = input("Why are you here? ")
+
+quote = "First do it, then do it right, then do it better"
+
+print(f"Hello {name}, it's nice to meet you! So you are {age} years old, that's cool ^^. \n Oh, so you're here because: {why}.")
+print("\nI have this quote that i strongly hold onto.\n{}".format(quote))
+
+message = "I hope to see you again."
+project_name = "Basic Python"
+project_number = "1"
+
+print(message)
+print(project_name + " " + project_number)
